1adf8c129b99a910518198e2db04a401f55f7cde
Seda805 <dudaeva44@gmail.com>
2021-09-09 22:04:36 +0300

    реализовала страницы вопросов

--- client/src/components/question-answer/questionsPage.jsx
+++ client/src/components/question-answer/questionsPage.jsx
@@ -1,39 +1,13 @@
-import React, { useState } from "react";
+import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
 import Paper from "@material-ui/core/Paper";
-import {
-  Box,
-  Button,
-  CardMedia,
-  IconButton,
-  InputAdornment,
-  TextField,
-  Typography,
-  withStyles,
-} from "@material-ui/core";
-import { purple } from "@material-ui/core/colors";
+import { Box, CardMedia, Typography } from "@material-ui/core";
 import DateRangeIcon from "@material-ui/icons/DateRange";
 import CommentIcon from "@material-ui/icons/Comment";
-import SearchIcon from "@material-ui/icons/Search";
-import CancelIcon from "@material-ui/icons/Cancel";
-import logo from "../../images/bg-header.svg";
-
-const ColorButton = withStyles((theme) => ({
-  root: {
-    color: theme.palette.getContrastText(purple[800]),
-    backgroundColor: purple[800],
-    "&:hover": {
-      backgroundColor: purple[900],
-    },
-  },
-}))(Button);
+import SearchBar from "./SearchBar";
 
 const useStyles = makeStyles((theme) => ({
-  root1: {
-    backgroundImage: `url(${logo})`,
-    backgroundColor: "red",
-  },
   paper: {
     flexGrow: 1,
     width: "90%",
@@ -65,61 +39,16 @@ const useStyles = makeStyles((theme) => ({
   login: {
     boxShadow: "0 1px grey",
   },
-  search: {
-    margin: theme.spacing(3, 0),
-  },
-  input: {
-    marginLeft: 80,
-  },
 }));
 
 export default function SpacingGrid() {
   const classes = useStyles();
 
-  const [value, setValue] = useState("");
   return (
     <Paper className={classes.paper}>
       {/* Здесь будет header */}
 
-      <Grid
-        container
-        display="flex"
-        justifyContent="space-between"
-        className={classes.search}
-      >
-        <Box flex="0.8">
-          <TextField
-            placeholder="Search"
-            type="text"
-            variant="outlined"
-            fullWidth
-            size="small"
-            className={classes.input}
-            onChange={(e) => setValue(e.target.value)}
-            value={value}
-            InputProps={{
-              startAdornment: (
-                <InputAdornment position="start">
-                  <SearchIcon />
-                </InputAdornment>
-              ),
-              endAdornment: value && (
-                <IconButton
-                  aria-label="toggle password visibility"
-                  onClick={() => setValue("")}
-                >
-                  <CancelIcon />
-                </IconButton>
-              ),
-            }}
-          />
-        </Box>
-        <Box>
-          <ColorButton variant="contained" color="primary">
-            Задать вопрос
-          </ColorButton>
-        </Box>
-      </Grid>
+      <SearchBar />
 
       <Grid container className={classes.question}>
         <Box
