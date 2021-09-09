9a19378fd57119f435a9c252ffd675c7099f0406
TheZiggie <85867091+TheZiggie@users.noreply.github.com>
2021-09-09 16:08:29 +0300

    Revert "Eva"

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ const { Router } = require("express");
 
 const router = Router();
 
-// router.use(require("./categories.route"));
-// router.use(require("./posts.route"));
-router.use(require("./users.route"));
+// router.use(require("categories.route"));
+// router.use(require("posts.route"));
+// router.use(require("users.route"));
 
 module.exports = router;
