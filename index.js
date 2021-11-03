ad0433fe2606ca90a17fd4083dd903223ec47f98
Seda805 <dudaeva44@gmail.com>
2021-11-03 13:17:12 +0300

    добавление NODE_ENV

--- index.js
+++ index.js
@@ -6,19 +6,21 @@ const path = require("path");
 require("dotenv").config();
 
 const app = express();
-const { PORT, MONGO_URI } = process.env;
+const { PORT, MONGO_URI, NODE_ENV } = process.env;
 
 app.use(fileUpload({}));
 //app.use("public", express.static(path.resolve(__dirname, "/client/public")));
-app.use(express.static(path.resolve(__dirname, "client", "build")));
+app.use(express.static(path.resolve(__dirname, "client", NODE_ENV === "production" ?  "build" : "public")));
 app.use(express.json());
 app.use(cors());
 
 app.use(require("./routes/index"));
 
+if (NODE_ENV === "production") {
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
+}
 
 const start = async () => {
   try {
