602e42b0207619bea649ec03ff456ada32c8509e
Seda805 <dudaeva44@gmail.com>
2022-01-10 16:23:14 +0300

    разбила файл на компоненты

--- client/src/components/Posts/AllPostsPage.jsx
+++ client/src/components/Posts/AllPostsPage.jsx
@@ -1,81 +1,85 @@
-import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import React from "react";
+import { useDispatch } from "react-redux";
 import { useHistory } from "react-router-dom";
 import { Box, CardMedia, Typography } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
-import { getAllPosts } from "../../redux/feautures/posts";
+import { getPostsByCategory } from "../../redux/feautures/posts";
 import chit from "../../images/horizontal_on_white_by_logaster.png";
 
 const useStyles = makeStyles((theme) => ({
-    category: {
-      width: "40%",
-      borderRadius: "5px",
-      color: "brown",
-      paddingTop: 4,
-      paddingBottom: 4,
-      cursor: "pointer",
-      backgroundColor: "grey",
-    },
-    avatar: {
-      width: 40,
-      height: 40,
-      borderRadius: "50%",
-    },
-    infoAuthor: {
-      position: "absolute",
-      bottom: "0%",
-      display: "flex",
-      borderTop: " 1px solid grey",
-      width: "100%",
-      p: "10px",
-    },
-    imglogo:{
-      width: '100%',
-      marginBottom: '16px !important'
-    }
-  }));
+  mainPost: {
+    marginLeft: 24,
+    marginBottom: "30px",
+    minHeight: 500,
+    position: "relative",
+    width: 500,
+  },
+  category: {
+    width: "40%",
+    borderRadius: "5px",
+    color: "brown",
+    paddingTop: 4,
+    paddingBottom: 4,
+    cursor: "pointer",
+    backgroundColor: "grey",
+  },
+  avatar: {
+    width: 40,
+    height: 40,
+    borderRadius: "50%",
+  },
+  infoAuthor: {
+    position: "absolute",
+    bottom: "0%",
+    display: "flex",
+    borderTop: " 1px solid grey",
+    width: "100%",
+    p: "10px",
+  },
+  imglogo: {
+    width: "100%",
+    marginBottom: "16px !important",
+  },
+}));
 
 function AllPostsPage({ post }) {
   const classes = useStyles();
   const dispatch = useDispatch();
   const history = useHistory();
 
-  const { items } = useSelector((store) => store.posts);
-
   const correctTime = (time) => `${new Date(time).toLocaleDateString()}`;
 
-  useEffect(() => {
-    dispatch(getAllPosts());
-  }, [dispatch]);
+  const handleGetByCategories = (categoryId) => {
+    dispatch(getPostsByCategory(categoryId));
+  };
 
   return (
     <>
-      {items.map((post) => (
-        <Box ml={3} mb="30px" minHeight={500} position="relative" width={500}>
-          <Box>
-            <img src={chit} className={classes.imglogo} alt="" />
-          </Box>
-          <Box display="flex" justifyContent="space-between">
-            <Box
-              className={classes.category}
-              onClick={() => history.push(`/posts/${post.category._id}`)}
-            >
-              {post.category.name}
-            </Box>
-            <Box>{correctTime(post.createdAt)}</Box>
-          </Box>
-          <Box pt={2} onClick={() => history.push(`/posts/${post._id}`)}>
-            <Typography variant="h5">{post.title}</Typography>
-          </Box>
-          <Box className={classes.infoAuthor}>
-            <CardMedia
-              image={post.author?.avatar_URI}
-              className={classes.avatar}
-            />
-            <Box p={1}>{post.author?.name || post.author?.login}</Box>
-          </Box>
+      <Box key={post._id} className={classes.mainPost}>
+        <Box>
+          <img src={chit} className={classes.imglogo} alt="" />
         </Box>
-      ))}
+        <Box display="flex" justifyContent="space-between">
+          <Box
+            className={classes.category}
+            onClick={() => handleGetByCategories(post?.category._id)}
+          >
+            {post.category?.name}
+          </Box>
+          <Box>{correctTime(post.createdAt)}</Box>
+        </Box>
+        <Box pt={2} onClick={() => history.push(`/post/${post._id}`)}>
+          <Typography variant="h5">{post.title}</Typography>
+        </Box>
+        <Box className={classes.infoAuthor}>
+          <CardMedia
+            image={post.author?.avatar_URI}
+            className={classes.avatar}
+            component="img"
+          />
+          <Box p={1}>{post.author?.name || post.author?.login}</Box>
+        </Box>
+      </Box>
     </>
   );
 }
--- client/src/components/Posts/PostPage.jsx
+++ client/src/components/Posts/PostPage.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { useHistory } from "react-router-dom";
-import { Button, CardMedia, Grid, Typography } from "@material-ui/core";
+import { Button, Grid, Typography } from "@material-ui/core";
 import { Box } from "@mui/system";
 import { Pagination } from "@mui/material";
 import { makeStyles } from "@material-ui/core/styles";
@@ -9,7 +8,6 @@ import Header from "./Header";
 import { getAllPosts,getCategories,getPostsByCategory } from "../../redux/feautures/posts";
 import logo from "../../images/bg-header.svg";
 import AllPostsPage from "./AllPostsPage";
-import chit from "../../images/horizontal_on_white_by_logaster.png";
 import SearchIn from "./Search";
 import CreatePost from "./CreatePost";
 
@@ -27,50 +25,14 @@ const useStyles = makeStyles((theme) => ({
     display: "flex",
     justifyContent: "space-around",
     marginTop: "50px",
-  },
-  mainPost: {
-    marginLeft: 24,
-    marginBottom: "30px",
-    minHeight: 500,
-    position: "relative",
-    width: 500,
-  },
-  category: {
-    width: "40%",
-    borderRadius: "5px",
-    color: "brown",
-    paddingTop: 4,
-    paddingBottom: 4,
-    cursor: "pointer",
-    backgroundColor: "grey",
-  },
-  avatar: {
-    width: 40,
-    height: 40,
-    borderRadius: "50%",
-  },
-  infoAuthor: {
-    position: "absolute",
-    bottom: "0%",
-    display: "flex",
-    borderTop: " 1px solid grey",
-    width: "100%",
-    p: "10px",
-  },
-  imglogo: {
-    width: "100%",
-    marginBottom: "16px !important",
-  },
+  }
 }));
 
 function PostPage() {
   const classes = useStyles();
   const dispatch = useDispatch();
-  const history = useHistory();
 
   const [textSearch, setTextSearch] = useState("");
-
-  const correctTime = (time) => `${new Date(time).toLocaleDateString()}`;
 
   const { items, categories, loading } = useSelector((store) => store.posts);
   const { text } = useSelector((store) => store.languages);
@@ -125,44 +87,11 @@ function PostPage() {
             </Box>
 
             <Box display="contents">
-              {/* {items.map((post) => {
-                return <AllPostsPage post={post} />;
-              })}   бесконечно рендерится если разбит на компонент*/}
-              {/* <AllPostsPage /> */}
               {items
-                ?.filter((item) =>
-                  item?.title?.toLowerCase().includes(textSearch.toLowerCase())
-                )
-                .map((post) => (
-                  <Box key={post._id} className={classes.mainPost}>
-                    <Box>
-                      <img src={chit} className={classes.imglogo} alt="" />
-                    </Box>
-                    <Box display="flex" justifyContent="space-between">
-                      <Box
-                        className={classes.category}
-                        onClick={() => handleGetByCategories(post?.category._id)}
-                      >
-                        {post.category?.name}
-                      </Box>
-                      <Box>{correctTime(post.createdAt)}</Box>
-                    </Box>
-                    <Box
-                      pt={2}
-                      onClick={() => history.push(`/post/${post._id}`)}
-                    >
-                      <Typography variant="h5">{post.title}</Typography>
-                    </Box>
-                    <Box className={classes.infoAuthor}>
-                      <CardMedia
-                        image={post.author?.avatar_URI}
-                        className={classes.avatar}
-                        component="img"
-                      />
-                      <Box p={1}>{post.author?.name || post.author?.login}</Box>
-                    </Box>
-                  </Box>
-                ))}
+                ?.filter((item) =>item?.title?.toLowerCase().includes(textSearch.toLowerCase()))
+                .map((post) => {
+                  return <AllPostsPage post={post} key={post._id} />;
+                })}
             </Box>
           </Grid>
 
